1e4d21741aac1e1b3994d004a41f9f237945c142
rohanmalhotra0 <rohanmalhotra8974@gmail.com>
2026-04-28 15:38:50 -0400

    Added global CSS

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify, render_template, redirect
 from pymongo import MongoClient
 from dotenv import load_dotenv
 import os
